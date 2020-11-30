3369d93c63ca8c8bd8b13b939fd4ef1dadada5c7
Yordan Dimitrov <dimitrovy@vmware.com>
2020-11-30 16:57:01 +0200

    onduty script

--- ical2csv.py
+++ ical2csv.py
@@ -4,21 +4,22 @@ import sys
 import os.path
 from icalendar import Calendar
 import csv
+import datetime
+from datetime import timedelta
+from dateutil import relativedelta
+from pytz import timezone
 
 filename = sys.argv[1]
 # TODO: use regex to get file extension (chars after last period), in case it's not exactly 3 chars.
 file_extension = str(sys.argv[1])[-3:]
-headers = ('Summary', 'UID', 'Description', 'Location', 'Start Time', 'End Time', 'URL')
+headers = ('OnDuty', 'Start Time', 'End Time')
+estzone = timezone('Europe/Sofia')
 
 class CalendarEvent:
     """Calendar event class"""
-    summary = ''
-    uid = ''
-    description = ''
-    location = ''
+    onduty = ''
     start = ''
     end = ''
-    url = ''
 
     def __init__(self, name):
         self.name = name
@@ -35,20 +36,12 @@ def open_cal():
 
             for component in gcal.walk():
                 event = CalendarEvent("event")
-                if component.get('TRANSP') == 'TRANSPARENT': continue #skip event that have not been accepted
-                if component.get('SUMMARY') == None: continue #skip blank items
-                event.summary = component.get('SUMMARY')
-                event.uid = component.get('UID')
-                if component.get('DESCRIPTION') == None: continue #skip blank items
-                event.description = component.get('DESCRIPTION')
-                event.location = component.get('LOCATION')
+                event.onduty = component.get('ATTENDEE')
                 if hasattr(component.get('dtstart'), 'dt'):
-                    event.start = component.get('dtstart').dt
+                    event.start = component.get('dtstart').dt.astimezone(estzone)
                 if hasattr(component.get('dtend'), 'dt'):
-                    event.end = component.get('dtend').dt
-
-
-                event.url = component.get('URL')
+                    event.end = component.get('dtend').dt.astimezone(estzone)
+                
                 events.append(event)
             f.close()
         else:
@@ -68,25 +61,36 @@ def csv_write(icsfile):
             wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
             wr.writerow(headers)
             for event in sortedevents:
-                values = (event.summary.encode('utf8').decode(), event.uid, event.description.encode('uft8').decode(), event.location, event.start, event.end, event.url)
+                values = (event.onduty, event.start, event.end)
                 wr.writerow(values)
             print("Wrote to ", csvfile, "\n")
     except IOError:
         print("Could not open file! Please close Excel!")
         exit(0)
 
+def filterNonWorkingHours(duty):
+    isWorkingHours = duty.start.date().weekday() not in [5,6] and duty.start.hour >= 8 and duty.end.hour <= 20
+    print(isWorkingHours, "\t| ", duty.start.date().weekday(), " | ", duty.start.hour, " | ", duty.end.hour, "\n")        
+    return not isWorkingHours
+    #endEst = estzone.localize(duty.start)
+    #return duties
+
+def filterSofia():
+    peopleToFilter = ["jveli@vmware.com", "mapostolova@vmware.com", "gbalabanov@vmware.com", "tjecheva@vmware.com", "tivanov@vmware.com"]
+    sofiaDuties = list(filter(lambda obj: obj.onduty in peopleToFilter, events))
+    filterFrom  = datetime.date(2020, 12, 1)
+    filterTo = filterFrom + relativedelta.relativedelta(months=1)
+    sofiaDutiesSelectedMonth = list(filter(lambda obj: obj.start.date() >= filterFrom and obj.start.date() < filterTo, sofiaDuties))
+    return list(filter(lambda obj: filterNonWorkingHours(obj), sofiaDutiesSelectedMonth))
 
 def debug_event(class_name):
     print("Contents of ", class_name.name, ":")
-    print(class_name.summary)
-    print(class_name.uid)
-    print(class_name.description)
-    print(class_name.location)
+    print(class_name.onduty)
     print(class_name.start)
-    print(class_name.end)
-    print(class_name.url, "\n")
+    print(class_name.end, "\n")
 
 open_cal()
-sortedevents=sorted(events, key=lambda obj: obj.start) # Needed to sort events. They are not fully chronological in a Google Calendard export ...
+sortedevents=filterSofia()
+#sortedevents=sorted(events, key=lambda obj: obj.start) # Needed to sort events. They are not fully chronological in a Google Calendard export ...
 csv_write(filename)
 #debug_event(event)
